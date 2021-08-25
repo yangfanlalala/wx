bfe59a9655a1fe287f54bdd9d3bae8b3dc349021
yangfan <yangfan@hualala.com>
2021-08-25 11:03:38 +0800

    nick_name

--- wxa_set_nickname.go
+++ wxa_set_nickname.go
@@ -31,7 +31,7 @@ func (client *WeChatClient) BuildWxaSetNicknameRequest() *WxaSetNicknameRequest 
 
 type WxaSetNicknameRequest struct {
 	AccessToken       string `position:"query" name:"access_token" json:"-"`
-	Nickname          string `position:"body" name:"nick_name" json:"nickname"`
+	Nickname          string `position:"body" name:"nick_name" json:"nick_name"`
 	IDCard            string `position:"body" name:"id_card" json:"id_card"`
 	License           string `position:"body" name:"license" json:"license"`
 	NamingOtherStuff1 string `position:"body" name:"naming_other_staff_1" json:"naming_other_stuff_1"`
