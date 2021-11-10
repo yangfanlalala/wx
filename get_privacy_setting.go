028e25a7df4cdcc7c46af6839c8683da4229939d
yf <yangfan.lalala@hotmail.com>
2021-11-10 21:03:19 +0800

    privacy

---
+++ get_privacy_setting.go
@@ -0,0 +1,43 @@
+package wx
+
+import "net/http"
+
+// API DOCUMENT https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/privacy_config/get_privacy_setting.html
+
+const APIGetPrivacySetting = "https://api.weixin.qq.com/cgi-bin/component/getprivacysetting"
+
+func (client *WeChatClient) GetPrivacySetting(data *GetPrivacySettingRequest) (*GetPrivacySettingResponse, error) {
+	req := &CommonRequest{}
+	req.WithURL(APIGetPrivacySetting).
+		WithContentType("application/json").
+		WithMethod(http.MethodPost).
+		WithData(data)
+	rsp := &struct {
+		CommonResponse
+		GetPrivacySettingResponse
+	}{}
+	if err := client.DoRequest(req, rsp); err != nil {
+		return nil, err
+	}
+	if err := rsp.Error(); err != nil {
+		return nil, err
+	}
+	return &rsp.GetPrivacySettingResponse, nil
+}
+
+func (client *WeChatClient) BuildGetPrivacySettingRequest() *GetPrivacySettingRequest {
+	return &GetPrivacySettingRequest{}
+}
+
+type GetPrivacySettingRequest struct {
+	AccessToken string `position:"query" name:"access_token" json:"-"`
+}
+
+type GetPrivacySettingResponse struct {
+	CodeExist    int64                 `json:"code_exist"`
+	PrivacyList  []string              `json:"privacy_list"`
+	SettingList  []*PrivacySettingItem `json:"setting_list"`
+	UpdateTime   int64                 `json:"update_time"`
+	OwnerSetting PrivacyOwnerSetting   `json:"owner_stting"`
+	PrivacyDesc  PrivacySettingDesc    `json:"privacy_desc"`
+}
--- set_privacy_setting.go
+++ set_privacy_setting.go
@@ -1,121 +1,147 @@
 package wx
 
-var PrivacyOptions = []*PrivacySettingItem{
+import "net/http"
+
+// API DOCUMENT https://developers.weixin.qq.com/doc/oplatform/Third-party_Platforms/2.0/api/privacy_config/set_privacy_setting.html
+
+const ApiSetPrivacySetting = "https://api.weixin.qq.com/cgi-bin/component/setprivacysetting"
+
+var PrivacyOptions = []*PrivacySettingDesc{
 	{
-		PrivacyKey: "UserInfo",
+		PrivacyKey:  "UserInfo",
 		PrivacyDesc: "用户信息（微信昵称、头像）",
 	},
 	{
-		PrivacyKey: "Location",
+		PrivacyKey:  "Location",
 		PrivacyDesc: "位置信息",
 	},
 	{
-		PrivacyKey: "Address",
+		PrivacyKey:  "Address",
 		PrivacyDesc: "地址",
 	},
 	{
-		PrivacyKey: "Invoice",
+		PrivacyKey:  "Invoice",
 		PrivacyDesc: "发票信息",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "RunData",
+		PrivacyDesc: "微信运动数据",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "Record",
+		PrivacyDesc: "麦克风",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "Album",
+		PrivacyDesc: "选中的照片或视频信息",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "Camera",
+		PrivacyDesc: "摄像头",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "PhoneNumber",
+		PrivacyDesc: "手机号码",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "Contact",
+		PrivacyDesc: "通讯录（仅写入）权限",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "DeviceInfo",
+		PrivacyDesc: "设备信息",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "EXIDNumber",
+		PrivacyDesc: "身份证号码",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "EXOrderInfo",
+		PrivacyDesc: "订单信息",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "EXUserPublishContent",
+		PrivacyDesc: "发布内容",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "EXUserFollowAcct",
+		PrivacyDesc: "所关注账号",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "EXUserOpLog",
+		PrivacyDesc: "操作日志",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "AlbumWriteOnly",
+		PrivacyDesc: "相册（仅写入）权限",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "LicensePlate",
+		PrivacyDesc: "车牌号",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "BlueTooth",
+		PrivacyDesc: "蓝牙",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "CalendarWriteOnly",
+		PrivacyDesc: "日历（仅写入）权限",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "Email",
+		PrivacyDesc: "邮箱",
 	},
 	{
-		PrivacyKey: "",
-		PrivacyDesc: "",
+		PrivacyKey:  "MessageFile",
+		PrivacyDesc: "选中的文件",
 	},
 }
 
-func SetPrivacySetting() {
+func (client *WeChatClient) SetPrivacySetting(data *SetPrivacySettingRequest) error {
+	req := &CommonRequest{}
+	req.WithURL(ApiSetPrivacySetting).
+		WithContentType("application/json").
+		WithMethod(http.MethodPost).
+		WithData(data)
+	rsp := &CommonResponse{}
+	if err := client.DoRequest(req, rsp); err != nil {
+		return err
+	}
+	if err := rsp.Error(); err != nil {
+		return err
+	}
+	return nil
+}
 
+func (client *WeChatClient) BuildSetPrivacySettingRequest() *SetPrivacySettingRequest {
+	return &SetPrivacySettingRequest{}
+}
+
+type SetPrivacySettingRequest struct {
+	AccessToken  string                `position:"query" name:"access_token" json:"-"`
+	OwnerSetting PrivacyOwnerSetting   `json:"owner_setting"`
+	SettingList  []*PrivacySettingItem `json:"setting_list"`
 }
 
 type PrivacyOwnerSetting struct {
-	ContactEmail string `json:"contact_email"`
-	ContactPhone string `json:"contact_phone"`
-	ContactQQ string `json:"contact_qq"`
-	ContactWeixin string `json:"contact_weixin"`
-	ExtFileMediaID string `json:"ext_file_media_id"`
-	NoticeMethod string `json:"notice_method"`
+	ContactEmail         string `json:"contact_email"`
+	ContactPhone         string `json:"contact_phone"`
+	ContactQQ            string `json:"contact_qq"`
+	ContactWeixin        string `json:"contact_weixin"`
+	ExtFileMediaID       string `json:"ext_file_media_id"`
+	NoticeMethod         string `json:"notice_method"`
 	StoreExpireTimestamp string `json:"store_expire_timestamp"`
 }
 
 type PrivacySettingItem struct {
-	PrivacyKey string `json:"privacy_key"`
-	PrivacyDesc string `json:"privacy_desc,omitempty"`
-	PrivacyText string `json:"privacy_text,omitempty"`
+	PrivacyKey   string `json:"privacy_key"`
+	PrivacyText  string `json:"privacy_text,omitempty"`
+	PrivacyLabel string `json:"privacy_label,omitempty"`
 }
 
-
-type SetPrivacySettingRequest struct {
-	AccessToken string `position:"query" name:"access_token" json:"-"`
-	OwnerSetting PrivacyOwnerSetting `json:"owner_setting"`
-	SettingList []*PrivacySettingItem `json:"setting_list"`
+type PrivacySettingDesc struct {
+	PrivacyKey  string `json:"privacy_key"`
+	PrivacyDesc string `json:"privacy_desc"`
 }
 
 type SetPrivacySettingResponse struct {
